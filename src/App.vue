8748520fe06f29fcec48e9e7485d7c3bfc53e4ad
seriouslysean <sean@codeserious.com>
2024-10-29 11:50:14 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>v-mode.trim</h1>
+  <fieldset>
+    <label>Standard Input</label>
+    <hr />
+    <input v-model.trim="inputValue" />
+    <hr />
+    <code>Current Value: "{{ inputValue }}"</code>
+  </fieldset>
+  <hr />
+  <fieldset>
+    <label>Custom Input</label>
+    <hr />
+    <BaseInput v-model.trim="inputValue" />
+    <hr />
+    <code>Current Value: "{{ inputValue }}"</code>
+  </fieldset>
+</template>
+
+<script>
+import BaseInput from './components/BaseInput.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BaseInput,
+  },
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <input ref="input" class="base-input" :foo="modelValue" @input="onInput" />
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    onInput(e) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
